06501bf95ee3f6084e87b47d923ffc902eaf62b7
gabrielbentog <bentogdev@gmail.com>
2023-12-12 16:01:10 -0300

    configure devise

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, # Save password
+          :registerable, # Edit User
+          :recoverable, # Recover password
+          :rememberable, # Rememberable user
+          :validatable, # Validations
+          :confirmable, # Confirm registration
+          :trackable # Logins count
+
+  include DeviseTokenAuth::Concerns::User
+end
