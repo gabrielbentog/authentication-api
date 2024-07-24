feb9d82153726f619c9e0b88a4378bc841cc095b
gabrielbentog <bentogdev@gmail.com>
2024-07-24 09:28:27 -0300

    feat(Authentication): finish authenticate

--- app/controllers/api/v1/authenticate_controller.rb
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -1,19 +1,13 @@
-class AuthenticateController < ApplicationController  
+class Api::V1::AuthenticateController < ApplicationController  
 
   def authenticate
     @user = User.find_by(email: params[:email])
     if @user && @user.valid_password?(params[:password])
-      @user.token = generate_token(@user)
+      token =  @user.create_new_auth_token
+      @user.token = token["Authorization"] if token      
       render json: UserSerializer.new(@user).serializable_hash.to_json
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
-  end
-
-  private
-
-  def generate_token(user)
-    payload = { user_id: user.id, exp: 2.seconds.from_now.to_i }
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end
