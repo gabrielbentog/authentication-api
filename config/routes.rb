bc10c4059a78991f690a7af6dd4810eb4ef4f66d
gabrielbentog <bentogdev@gmail.com>
2024-07-24 09:31:11 -0300

    fix: devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
       get "up" => "rails/health#show", as: :rails_health_check
 
       post "authenticate" =>"authenticate#authenticate"
